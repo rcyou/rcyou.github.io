dd783e886790e757ed24b7ba4d1cd03b5ead6b80
Ryan Young <ryoung@mail.com>
2019-04-27 09:23:26 -0500

    more outbound link tracking cleanup

--- js/main.js
+++ js/main.js
@@ -53,8 +53,7 @@ var getOutboundLink = function(url) {
   gtag('event', 'click', {
     'event_category': 'outbound',
     'event_label': url,
-    'transport_type': 'beacon',
-    'event_callback': function(){document.location = url;}
+    'transport_type': 'beacon'
   });
 }
 
