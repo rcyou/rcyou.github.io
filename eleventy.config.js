1e5737e635e22a7c0032954f5251404298ec042e
Ryan Young <ryoung.hello@gmail.com>
2026-05-02 16:06:11 -0500

    deploying new 11ty site

---
+++ eleventy.config.js
@@ -0,0 +1,45 @@
+import path from "node:path";
+import * as sass from "sass";
+
+export default function(eleventyConfig) {
+  eleventyConfig.addTemplateFormats("scss");
+  
+  eleventyConfig.addExtension("scss", {
+      outputFileExtension: "css",
+
+      // opt-out of Eleventy Layouts
+      useLayouts: false,
+
+      compile: async function (inputContent, inputPath) {
+        let parsed = path.parse(inputPath);
+        // Don’t compile file names that start with an underscore
+        if(parsed.name.startsWith("_")) {
+          return;
+        }
+
+        let result = sass.compileString(inputContent, {
+          loadPaths: [
+            parsed.dir || ".",
+            this.config.dir.includes,
+          ]
+        });
+
+        // Map dependencies for incremental builds
+        this.addDependencies(inputPath, result.loadedUrls);
+
+        return async (data) => {
+          return result.css;
+        };
+      },
+    });
+
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
